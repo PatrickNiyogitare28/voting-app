734036e9ffce8f361c2c07c43f037a92b3dcc3c2
Patrick Niyogitare <54368821+PatrickNiyogitare28@users.noreply.github.com>
2022-07-07 00:00:53 +0200

    Update index.js

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,1 +1,1 @@
-export const API_URL = 'http://server:5000/api/v1';
+export const API_URL = 'http://localhost:5000/api/v1'; // 'http://sever/api/v1' when need to connect to container as a backend service
